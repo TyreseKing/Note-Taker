50ec4e2cb75af2a89bf85ffb56f5bd5909b0c904
King <Tyreseking11@gmail.com>
2023-01-27 11:50:42 -0500

    v1

--- server.js
+++ server.js
@@ -1,78 +1,69 @@
 const express = require("express");
 const path = require("path");
-const notes = require("./db/db.json");
+const dataBase = require("./db/db.json");
 const fs = require("fs");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Get = READ - Post = CREATE - Put = UPDATE - Delete -= DELETE
-
-
-//GET route for homepage
-app.get("/", (req, res) => {
+// HTML routes
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
-  console.log("index.html route working")
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// API routes for notes page
+app.get("/api/notes", (req, res) => {
+  res.json(dataBase.slice(1));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-    console.log("notes.html route working")
-})
-
-// API route for getting notes - This gets all the json(db) to show our notes
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-    console.log(notes)
-})
-
-// // API route for creating notes - This handles creating a note and adding it to json(db)
-// app.post('/api/notes', (req, res) => {
-//     const postNote = createNote(req.body, notes)
-//     res.json(postNote)
-//     console.log("POST METHOD IS WORKING")
-// })
-
-// // Function that actually handles creating note with id and updating our db file
-// const createNote = (body, notesArr) => {
-//     const postNote = body;
-//     body.id = notesArr.length;
-//     notesArr.push(postNote)
-//     fs.writeFileSync(
-//         path.join(__dirname, "./db/db.json"),
-//         JSON.stringify(notesArr, 2)
-//     )
-//     return postNote
-// }
-
-// //  API route for deleting notes - This deletes a note by id
-// app.delete('/api/notes/:id', (req, res) => {
-//     deleteNote(req.params.id)
-//     res.json(true)
-//     console.log("DELETE METHOD IS WORKING")
-// })
-
-// const deleteNote = (noteId) => {
-//     for(let i = 0; i < notes.length; i++) {
-//         let note = notes[i]
-//         if (note.id == noteId) {
-//             notes.splice(i, 1)
-//             fs.writeFileSync(
-//                 path.join(__dirname, "./db/db.json"),
-//                 JSON.stringify(notes)
-//             )
-//             break;
-//         } else {
-//             console.log("No note matches that id")
-//         }
-//     }
-// }
-
-
-app.listen(PORT, ()=> {
-    console.log(`listening on PORT: ${PORT}`)
-})
+app.post("/api/notes", (req, res) => {
+  const newNote = createNote(req.body, dataBase);
+  res.json(newNote);
+});
+// Function to create new note and count ID for each note
+const createNote = (body, notesArray) => {
+    const newNote = body;
+    let notesArray = [];
+  
+    body.id = notesArray.length;
+    notesArray.push(newNote);
+  
+    fs.writeFileSync(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+  };
+  
+  app.delete("/api/notes/:id", (req, res) => {
+    deleteNote(req.params.id, dataBase);
+    res.json(true);
+  });
+  // Function to delete notes if user decided to
+  const deleteNote = (id, notesArray) => {
+    for (let i = 0; i < notesArray.length; i++) {
+      let note = notesArray[i];
+      if (note.id == id) {
+        notesArray.splice(i, 1);
+        fs.writeFileSync(
+          path.join(__dirname, "./db/db.json"),
+          JSON.stringify(notesArray, null, 2)
+        );
+        break;
+      }
+    }
+  };
+  
+  // App listenening at http://localhost:3001
+  app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+  
